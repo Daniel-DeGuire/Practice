7c997816f71ac3a4f93b8560c5257caa19924dfe
Daniel DeGuire <88063253+Daniel-DeGuire@users.noreply.github.com>
2021-08-24 15:35:10 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,1 +1,17 @@
+const pizzaToppings = ["Pepperoni", "Sausage", "Chicken", "Bacon"];
+const pizzaCount = ["One", "Two", "Three"]
+const pizzaCrust = ["Thick", "Thin"];
+const pizzaSize = ["Large", "Medium"];
+const orderText = "pizza ready shorty.";
 
+let greetCustomer = "Welcome to Pizza House our toppings are: ";
+for (let i = 0; i < pizzaToppings.length; i++) {
+  greetCustomer += pizzaToppings[i] + "<br>";
+}
+
+document.getElementById("demo").innerHTML = greetCustomer;
+
+let orderFood = pizzaCount[0] + " " + pizzaCrust[0] + " " + pizzaToppings[0] + " " + orderText;
+
+document.getElementById("demotwo").innerHTML = orderFood;
+</script>
